fbad93ad555e67aa6276a84326a1a5e9b24d287d
Developer Dave <116127714+Developer-Dave1@users.noreply.github.com>
2025-12-15 12:24:33 -0500

    adding guess count feature

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function isValidInteger(num) {
+  if (!Number.isInteger(num)) return false;
+  if (typeof num !== 'number') return false; 
+  if (String(num).length > 3) return false;
+
+  return true;
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
   let answer = getRandomNumber(1, 100);
   let form = document.querySelector('#inputForm');
@@ -11,17 +19,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
   let guessParagraph = document.querySelector('#guessParagraph');
   let guessCount = 0;
 
+
     form.addEventListener('submit', event => {
       event.preventDefault();
+      if (!isValidInteger(input.value)) {
+        message.textContent = 'Invalid input. Please enter a three-digit number.';
+      } 
+      
       let guess = parseInt(input.value, 10);
-      guessCount ++;
-      guessParagraph.textContent = `Guess Count: ${guessCount}`;
+      
       if (guess > answer) {
         message.textContent = `The answer is lower than ${guess}`;
+        guessCount ++;
       } else if (guess < answer) {
         message.textContent = `The answer is higher than ${guess}`;
-      } else  if (guess = answer) { 
+        guessCount ++;
+      } else  if (guess === answer) { 
+        guessCount ++;
         message.textContent = `You guessed correctly!`;
+        guessParagraph.textContent = `It took you ${guessCount} guesses to get the correct answer.`
       }
 
     });
@@ -30,6 +46,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
       event.preventDefault();
       answer = getRandomNumber(1, 100);
       input.value = "";
+      guessParagraph.textContent = '';
+      guessCount = 0;
       message.textContent = 'Guess a number from 1 to 100';
     });
 
